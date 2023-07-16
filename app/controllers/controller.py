b0a981d9215bb9afd9105e38604730df067dbaa0
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-07-17 02:10:15 +0400

    Delete controller.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- app/controllers/controller.py
+++
@@ -1,9 +0,0 @@
-import random
-from string import ascii_letters, digits, punctuation
-
-def generate_random_password(lenght:int)-> str:
-    """Generate random string"""
-    characters = str(ascii_letters + digits + punctuation)
-    print(characters)
-    password = ''.join(random.choice(characters) for _ in range(lenght))
-    return  password
