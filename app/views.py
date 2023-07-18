c4551c0c53c4217217d2b49c944ffa049ea8512e
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-07-18 23:46:25 +0400

    ревью
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- app/views.py
+++ app/views.py
@@ -1,17 +1,16 @@
-from flask import Flask, render_template, request
+from flask import render_template, request
 from controllers import generate_random_password
-
-app = Flask(__name__)
+здесь нужно отимпортить экземпляр класса 
 
 @app.route('/')
 def home():
-    return render_template('app\static\index.html')
+    return render_template('index.html')
 
 @app.route('/generate', method=['POST'])
 def generate_password():
     length =  int(request.form['lenght'])
     password = generate_random_password(length)
-    return render_template('app\static\index.html', password=password)
+    return render_template('index.html', password=password)
 
 
 
