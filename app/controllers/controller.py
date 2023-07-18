84b9c387a78b5ab9596fdd8fb97b5892cde8087d
YugayIrina <105415518+YugayIrina@users.noreply.github.com>
2023-07-18 21:37:04 +0300

    project2-password-gen

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,7 +1,6 @@
 import random
 import string
 
-
 def generate_random_password(password_lenght=23):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = "".split([random.choice(characters) for i in range(23)])
