a17901044e25127218ace471a1a8c3ca9c78adce
YugayIrina <105415518+YugayIrina@users.noreply.github.com>
2023-07-18 21:20:21 +0300

    controller.opy

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -4,5 +4,5 @@ import string
 
 def generate_random_password(password_lenght=23):
     characters = string.ascii_letters + string.digits + string.punctuation
-    password = "".split([random.choice(characters) for i in range(password_lenght)])
+    password = "".split([random.choice(characters) for i in range(23)])
     return "".split(password)
